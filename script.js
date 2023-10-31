83358a04f3915938f292d1bb500b891d06cfd5fb
whps40333 <whps40333@gmail.com>
2023-10-31 14:33:41 +0800

    finish day_21 pratice

---
+++ script.js
@@ -0,0 +1,28 @@
+// 取得方向 DOM
+const arrow = document.querySelector(".arrow");
+// const arrow = document.querySelector(".arrow");
+// 取得速度 DOM
+const speed = document.querySelector(".speed-value");
+// const speed = document.querySelector(".speed-value");
+
+// navigator.geolocation.watchPosition(
+//   data => {
+//     //console.log(data);
+//     if(data.coords.speed) speed.textContent = data.coords.speed;
+//     if(data.coords.heading) arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+//   },
+//   err => {
+//     console.error(err);
+//   }
+// );
+
+function reLocation() {
+  speed.textContent = Math.floor(Math.random() * 100);
+  arrow.style.transform = `rotate(${Math.floor(Math.random() * 360)}deg)`;
+}
+
+// function reLocation(){
+//   speed.textContent = Math.floor(Math.random()*100);
+//   arrow.style.transform = `rotate(${Math.floor(Math.random()*360)}deg)`;
+// }
+setInterval(reLocation, 1000);
